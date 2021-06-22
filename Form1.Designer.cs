986105dd8a6925fbcd6dc23095d8cc9f27ed3d6b
OleksPal <oleksandr.palamarchuk1310@gmail.com>
2021-06-22 15:36:28 +0300

    commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿
+namespace Obfuscation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quitToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.обфускаціяToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aBOUTToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutProgramToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.quitToolStripMenuItem,
+            this.quitToolStripMenuItem1});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(73, 26);
+            this.файлToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(199, 26);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // quitToolStripMenuItem
+            // 
+            this.quitToolStripMenuItem.Name = "quitToolStripMenuItem";
+            this.quitToolStripMenuItem.Size = new System.Drawing.Size(199, 26);
+            this.quitToolStripMenuItem.Text = "Save As...";
+            this.quitToolStripMenuItem.Click += new System.EventHandler(this.quitToolStripMenuItem_Click);
+            // 
+            // quitToolStripMenuItem1
+            // 
+            this.quitToolStripMenuItem1.Name = "quitToolStripMenuItem1";
+            this.quitToolStripMenuItem1.Size = new System.Drawing.Size(199, 26);
+            this.quitToolStripMenuItem1.Text = "Quit";
+            this.quitToolStripMenuItem1.Click += new System.EventHandler(this.quitToolStripMenuItem1_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Font = new System.Drawing.Font("NFS font", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem,
+            this.обфускаціяToolStripMenuItem,
+            this.aBOUTToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(7, 3, 0, 3);
+            this.menuStrip1.Size = new System.Drawing.Size(1182, 32);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // обфускаціяToolStripMenuItem
+            // 
+            this.обфускаціяToolStripMenuItem.Name = "обфускаціяToolStripMenuItem";
+            this.обфускаціяToolStripMenuItem.Size = new System.Drawing.Size(152, 26);
+            this.обфускаціяToolStripMenuItem.Text = "Obfuscate";
+            this.обфускаціяToolStripMenuItem.Click += new System.EventHandler(this.обфускаціяToolStripMenuItem_Click);
+            // 
+            // aBOUTToolStripMenuItem
+            // 
+            this.aBOUTToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutProgramToolStripMenuItem});
+            this.aBOUTToolStripMenuItem.Name = "aBOUTToolStripMenuItem";
+            this.aBOUTToolStripMenuItem.Size = new System.Drawing.Size(81, 26);
+            this.aBOUTToolStripMenuItem.Text = "Help";
+            // 
+            // aboutProgramToolStripMenuItem
+            // 
+            this.aboutProgramToolStripMenuItem.Name = "aboutProgramToolStripMenuItem";
+            this.aboutProgramToolStripMenuItem.Size = new System.Drawing.Size(291, 26);
+            this.aboutProgramToolStripMenuItem.Text = "About program...";
+            this.aboutProgramToolStripMenuItem.Click += new System.EventHandler(this.aboutProgramToolStripMenuItem_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.textBox1.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.textBox1.Location = new System.Drawing.Point(7, 35);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.ShortcutsEnabled = false;
+            this.textBox1.Size = new System.Drawing.Size(580, 640);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TabStop = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.textBox2.Cursor = System.Windows.Forms.Cursors.Default;
+            this.textBox2.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.textBox2.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.textBox2.Location = new System.Drawing.Point(597, 35);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(580, 640);
+            this.textBox2.TabIndex = 4;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.BackColor = System.Drawing.Color.MediumBlue;
+            this.progressBar1.Location = new System.Drawing.Point(7, 681);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(1170, 23);
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.ClientSize = new System.Drawing.Size(1182, 703);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Consolas", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(1200, 750);
+            this.Name = "Form1";
+            this.Text = "Symbolic Obfuscator";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem quitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem quitToolStripMenuItem1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem обфускаціяToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ToolStripMenuItem aBOUTToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutProgramToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,342 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Obfuscation
+{
+    public partial class Form1 : Form
+    {
+        bool isOpen = false, isObfuscated = false;
+        string comPath;
+        string fileNameExt;
+
+        List<String> variables = new List<String> ();
+        string[] exeptStrings = { "using", "namespace", "static", "class", "new", "else",
+        "return", "protected", "public" };
+        string[] dataTypes = { "var", "void", "int", "char", "bool", "String", "string", "float", "double" };
+
+        public Form1()
+        {
+            InitializeComponent();
+            StartPosition = FormStartPosition.CenterScreen;
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var fileContent = string.Empty;
+            var filePath = string.Empty;
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = @"c:\\Users\Oleksandr\Desktop";
+                openFileDialog.Filter = "CS files (*.cs)|*.cs|C++ files (*.cpp)|*.cpp";
+                openFileDialog.FilterIndex = 1;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Get the path of specified file
+                    filePath = openFileDialog.FileName;
+                    comPath = filePath;
+
+                    //Read the contents of the file into a stream
+                    var fileStream = openFileDialog.OpenFile();
+                    fileNameExt = openFileDialog.SafeFileName;
+
+                    using (StreamReader reader = new StreamReader(fileStream))
+                    {
+                        fileContent = reader.ReadToEnd();
+                        textBox1.Text = fileContent;
+                        isOpen = true;
+                    }
+                    textBox2.Text = " ";
+                }
+            }
+        }
+
+        private void ObfusAll() 
+        {
+            string formatText = textBox1.Text;
+            int start;
+            while (formatText.Contains("//"))
+            {
+                start = formatText.IndexOf("//");
+                formatText = formatText.Remove(start,
+                    formatText.IndexOf(Environment.NewLine, start) - start + 2);
+            }
+            progressBar1.PerformStep();
+            while (formatText.Contains("/*"))
+            {
+                start = formatText.IndexOf("/*");
+                formatText = formatText.Remove(start,
+                    formatText.IndexOf("*/", start) - start + 2);
+            }
+            progressBar1.PerformStep();
+
+            FindClasses(formatText);
+            progressBar1.PerformStep();
+            for (int i = 0; i < dataTypes.Length - 1; i++)
+            {
+                FindVars(formatText, dataTypes[i]);
+                FindArrs(formatText, dataTypes[i]);
+            }
+            for (int i = 0; i < variables.Count - 1; i++) {
+                if (variables[i].Contains("(") || variables[i] == "")
+                    variables.RemoveAt(i);
+            }
+            progressBar1.PerformStep();
+
+            for (int i = 0; i < variables.Count-1; i++)
+            {
+                Random rnd = new Random();
+                String addVal = "", previousNewValue = "", newValue = genName();
+                if (newValue == previousNewValue)
+                {
+                    addVal = ((char)rnd.Next(97, 123)).ToString();
+                    newValue += addVal;
+                }
+                if (i / variables.Count == 0)
+                    progressBar1.PerformStep();
+                if (i+1 / variables.Count == 1)
+                    progressBar1.PerformStep();
+                if (formatText.Contains(variables[i]))
+                    formatText = formatText.Replace(variables[i], previousNewValue = newValue);
+            }
+            progressBar1.PerformStep();
+            progressBar1.PerformStep();  
+
+            formatText = regularStrings(formatText);
+            formatText = formatText.Replace(Environment.NewLine, String.Empty);
+            progressBar1.PerformStep();
+
+            for (int i = 0; i < exeptStrings.Length; i++)
+                formatText = execeptions(formatText, exeptStrings[i]);
+            progressBar1.PerformStep();
+            for (int i = 0; i < dataTypes.Length; i++)
+                formatText = execeptions(formatText, dataTypes[i]);
+            textBox2.Text = formatText;
+            progressBar1.PerformStep();
+            isObfuscated = true;
+        }
+
+        private string execeptions(string mainString, string exeption)
+        {
+            String noR = mainString, keyword = exeption; 
+            if (noR.Contains(keyword))
+                noR = noR.Replace(keyword, keyword + " ");
+
+            return noR;
+        }
+
+        private string genName() {
+            Random rnd = new Random();
+            Thread.Sleep(50);
+            int way, length = rnd.Next(1, 7);
+            int preResult;
+            String result = "";
+
+            for (int i = 0; i < length; i++)
+            {
+                way = rnd.Next(1, 3);
+                if (way==1)
+                    preResult = rnd.Next(65, 91);
+                else
+                    preResult = rnd.Next(97, 123);
+                result += (char) preResult;
+            }
+            return result;
+        }
+
+        private void FindVars(string origin, string type) 
+        {
+            String noR = origin;
+            String ftype = " " + type + " ";
+            if (noR.Contains(ftype))
+            {
+                String newStr = noR;
+                String s1 = "";
+
+                while (newStr.Contains(ftype))
+                {
+                    s1 = newStr.Substring(newStr.IndexOf(ftype) + type.Length + 2,
+                        newStr.IndexOf("=", newStr.IndexOf(ftype)) - newStr.IndexOf(ftype));
+                    s1 = s1.Replace(";", " ");
+                    s1 = s1.Replace(")", " ");
+                    s1 = s1.Replace("(", " ");
+                    s1 = s1.Replace("[", " ");
+                    s1 = s1.Replace("]", " ");
+                    s1 = s1.Replace(Environment.NewLine, " ");
+                    s1 = s1.Substring(0, s1.IndexOf(" "));
+                    if (s1 != "Main")
+                        variables.Add(s1);
+                    newStr = newStr.Substring(newStr.IndexOf(ftype) + 5);               
+                }
+            }
+        }
+
+        private void FindClasses(string origin)
+        {
+            String noR = origin;
+            if (noR.Contains(" "+"class"+" "))
+            {
+                String newStr = noR;
+                String s1 = "";
+
+                while (newStr.Contains(" " + "class" + " "))
+                {
+                    s1 = newStr.Substring(newStr.IndexOf(" " + "class" + " ") + 5 + 2,
+                        newStr.IndexOf("(", newStr.IndexOf(" " + "class" + " ")) - newStr.IndexOf(" " + "class" + " "));
+                    s1 = s1.Replace("(", " ");
+                    s1 = s1.Replace(Environment.NewLine, " ");
+                    s1 = s1.Substring(0, s1.IndexOf(" "));
+                    variables.Add(s1);
+                    newStr = newStr.Substring(newStr.IndexOf(" " + "class" + " ") + 5);
+                }
+            }
+        }
+
+        private void FindArrs(string origin, string type) 
+        {
+            String noR = origin;
+            String ftype = " " + type;
+            if (noR.Contains(ftype))
+            {
+                String newStr = noR;
+                String s1 = "";
+
+                if (newStr.Contains(type + "[]") || newStr.Contains(type + " []"))
+                {
+                    while (newStr.Contains(ftype) || newStr.Contains(type + "[]"))
+                    {
+                        s1 = newStr.Substring(newStr.IndexOf(ftype) + type.Length + 4,
+                            newStr.IndexOf("=", newStr.IndexOf(ftype)) - newStr.IndexOf(ftype));
+                        s1 = s1.Replace("[", " ");
+                        s1 = s1.Replace(",", " ");
+                        s1 = s1.Replace(";", " ");
+                        s1 = s1.Replace("]", " ");
+                        s1 = s1.Substring(0, s1.IndexOf(" "));
+                        variables.Add(s1);
+                        newStr = newStr.Substring(newStr.IndexOf(ftype) + 5);
+                    }
+                }
+            }
+        }
+
+        private string regularStrings(string origin)
+        {
+            String noObfStr = "", noR = origin; 
+            int start, finish;
+
+            if (noR.Contains(" "))
+            {
+                String s1 = "", newStr = noR;
+                noR = "";
+
+                while (newStr.Contains(" ") || newStr.Contains("\""))
+                {
+                    mark: 
+                    noR += deleteSpace(s1) + noObfStr;
+                    if (newStr.Contains("\"") || newStr.Contains(" "))
+                    {
+                        start = newStr.IndexOf("\"");
+                        finish = newStr.IndexOf("\"", start + 1);
+                        if (start >= 0 && finish > 0)
+                            noObfStr = newStr.Substring(start, finish - start + 1);
+                        else
+                            noObfStr = "";
+
+                        if (!newStr.Contains("\""))
+                        {
+                            s1 = newStr.Substring(0);
+                            newStr = "";
+                            goto mark;
+                        }
+                        else
+                        {
+                            s1 = newStr.Substring(0, newStr.IndexOf(noObfStr));
+                            newStr = newStr.Substring(newStr.IndexOf(noObfStr)-1 + noObfStr.Length + 1);
+                        }
+                    }
+                    else
+                        break;
+                }
+            }
+            return noR;
+        }
+
+        private string deleteSpace(string origin) {
+            String source = origin;
+            source = source.Replace(" ", String.Empty);
+            source = Regex.Replace(source, @"\s", "");
+
+            return source;
+        }
+
+        private void обфускаціяToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            progressBar1.Value = 0;
+            if (isOpen)
+            {
+                ObfusAll();
+            }
+            else
+                MessageBox.Show("Open file first", "Abort", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+
+        private void quitToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private async void quitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (isOpen && isObfuscated)
+            {
+                SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+
+                saveFileDialog1.Filter = "CS files (*.cs)|*.cs|C++ files (*.cpp)|*.cpp";
+                saveFileDialog1.FilterIndex = 1;
+                saveFileDialog1.RestoreDirectory = true;
+                saveFileDialog1.FileName = "Obf-" + fileNameExt;
+                saveFileDialog1.AddExtension = true;
+
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    if (saveFileDialog1.FilterIndex == 1)
+                        saveFileDialog1.DefaultExt = ".cs";
+                    else
+                        saveFileDialog1.DefaultExt = ".cpp";
+                    // создаем каталог для файла
+                    string path = comPath.Substring(0, comPath.LastIndexOf("\\"));
+                    DirectoryInfo dirInfo = new DirectoryInfo(path);
+                    if (!dirInfo.Exists)
+                    {
+                        dirInfo.Create();
+                    }
+
+                    // запись в файл
+                    string fullDirWrite = saveFileDialog1.FileName;
+                    using (FileStream fstream = new FileStream(fullDirWrite, FileMode.OpenOrCreate))
+                    {
+                        byte[] array = System.Text.Encoding.Default.GetBytes(textBox2.Text);
+                        // асинхронная запись массива байтов в файл
+                        await fstream.WriteAsync(array, 0, array.Length);
+                    }
+                }
+            }
+            else if (!isOpen)
+                MessageBox.Show("Open file first", "Abort", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            else if (!isObfuscated)
+                MessageBox.Show("Obfuscate program first", "Abort", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+
+        private void aboutProgramToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Symbolic Obfuscator v 1.0\nCopyright ©  2021 Oleksandr Palamarchuk\n" +
+                "oleksandr.palamarchuk1310@gmail.com", "About", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
